1409d46b8d0b4eac483a54cc15a27315dea3413e
Test Saha <test.saha@test.com>
2023-07-27 10:38:09 +0530

    test commit

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def swagger():
-    print('sending root')
+    print('sending root test api')
     return render_template('swaggerui.html')
 
 #LANclients API
